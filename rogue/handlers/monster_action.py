005284582996f93bf88938e26381cc14ca322ab2
Matt Lebrun <matt.l@lead-surf.com>
2017-04-15 14:53:35 +0800

    Used the the take turn move

--- rogue/handlers/monster_action.py
+++ rogue/handlers/monster_action.py
@@ -9,4 +9,4 @@ log = logging.getLogger('default')
 
 def process(dungeon):
     for ent in dungeon.entities.monsters:
-        log.info('The {} growls!'.format(ent.name.title()))
+        ent.ai.take_turn()
